fef9c2fca0b890fdd5b708a0ab24138b27217bb8
unknown <rafay.work106@gmail.com>
2023-06-08 20:10:11 +0530

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
@@ -18,6 +19,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Envirement is set to Development"));
+}
 
 app.use(errorHandler);
 
--- frontend/src/pages/UpdateGoal.jsx
+++ frontend/src/pages/UpdateGoal.jsx
@@ -1,14 +1,10 @@
-// import { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { useNavigate, useParams } from "react-router-dom";
-// import { updateGoal } from "../features/goals/goalSlice";
 import GoalForm from "../components/GoalForm";
 
 function UpdateGoal() {
   return (
     <div>
       <h2>Update your goal</h2>
-      <GoalForm />
+      <GoalForm btnTxt="Update Goal" />
     </div>
   );
 }
